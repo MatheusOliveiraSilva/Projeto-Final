9e3faeaf86aa8479983faf5e1cab211a1c89c007
Matheus Oliveira Silva <matheus@cogg.ai>
2023-09-16 17:46:44 -0300

    arquivo preprocesser + novo arquivo versão2

---
+++ implementations/preprocessment/imagenetPreprocesser.py
@@ -0,0 +1,65 @@
+# Arquivo de processamento de dados, contendo as funções de preprocessamento de dados para o dataset ImageNet.
+# O dataset ImageNet é um dataset de imagens de 1000 classes, com 1000 imagens por classe, totalizando 1 milhão de imagens. Mas 
+# diferente dos outros datasets, nem todas imagens são de dimensões iguais, e nem todas imagens são coloridas. Por isso, é necessário
+# um preprocessamento diferente para esse dataset.
+
+
+import os
+import random
+from PIL import Image
+import shutil
+
+def sample_imagenet_data(root_dir, output_dir, n_samples=100000, split_ratios=(0.7, 0.2, 0.1), target_size=(256, 256)):
+    """
+    Amostra e organiza o dataset ImageNet.
+    
+    Parâmetros:
+        root_dir (str): Diretório raiz do ImageNet
+        output_dir (str): Diretório de saída para as amostras
+        n_samples (int): Número total de imagens a serem amostradas
+        split_ratios (tuple): Proporção de divisão para treino, validação e teste (deve somar 1.0)
+        target_size (tuple): Resolução desejada das imagens
+    """
+    
+    # Verifica os proporções
+    assert sum(split_ratios) == 1.0, "As proporções de treino, validação e teste devem somar 1.0"
+    
+    # Cria os diretórios de saída
+    train_dir = os.path.join(output_dir, 'train')
+    val_dir = os.path.join(output_dir, 'val')
+    test_dir = os.path.join(output_dir, 'test')
+    for dir in [train_dir, val_dir, test_dir]:
+        if not os.path.exists(dir):
+            os.makedirs(dir)
+
+    # Obtém a lista completa de imagens
+    all_images = []
+    for subdir, dirs, files in os.walk(root_dir):
+        for file in files:
+            if file.endswith(('.JPEG', '.jpg', '.png')):
+                all_images.append(os.path.join(subdir, file))
+    
+    # Amostra aleatoriamente n_samples imagens
+    sampled_images = random.sample(all_images, n_samples)
+    
+    # Divide as imagens nos conjuntos de treino, validação e teste
+    n_train = int(n_samples * split_ratios[0])
+    n_val = int(n_samples * split_ratios[1])
+    
+    train_images = sampled_images[:n_train]
+    val_images = sampled_images[n_train:n_train+n_val]
+    test_images = sampled_images[n_train+n_val:]
+    
+    # Função auxiliar para redimensionar e salvar imagens
+    def process_and_save(images, output_folder):
+        for img_path in images:
+            img = Image.open(img_path)
+            img = img.resize(target_size)
+            img.save(os.path.join(output_folder, os.path.basename(img_path)))
+    
+    process_and_save(train_images, train_dir)
+    process_and_save(val_images, val_dir)
+    process_and_save(test_images, test_dir)
+
+# Exemplo de uso:
+# sample_imagenet_data('/path/to/imagenet', '/path/to/output', n_samples=100000)
